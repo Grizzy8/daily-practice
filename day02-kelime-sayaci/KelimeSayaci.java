059f6761ac9eade611cef9095235d047d4fa39b0
Sezer Çetinkaya <sezercetinkaya24@gmail.com>
2026-07-27 23:52:21 +0300

    refactor: day02 metotlara ayrildi, esit uzunluktaki tum en uzun kelimeler listelenecek sekilde guncellendi

--- day02-kelime-sayaci/KelimeSayaci.java
+++ day02-kelime-sayaci/KelimeSayaci.java
@@ -1,27 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class KelimeSayaci {
+
+    public static String[] temizleVeAyir(String cumle) {
+        return cumle.replaceAll("[.,'*!?]", "").split("\\s+");
+    }
+
+    public static ArrayList<String> enUzunKelimelerBul(String[] kelimeler) {
+        ArrayList<String> enUzunKelimeler = new ArrayList<>();
+        int enUzunUzunluk = kelimeler[0].length();
+        for (int i = 0; i < kelimeler.length; i++) {
+            if (enUzunUzunluk < kelimeler[i].length()) {
+                enUzunKelimeler.clear();
+                enUzunUzunluk = kelimeler[i].length();
+                enUzunKelimeler.add(kelimeler[i]);
+            } else if (enUzunUzunluk == kelimeler[i].length()) {
+                enUzunKelimeler.add(kelimeler[i]);
+            }
+        }
+        return enUzunKelimeler;
+    }
+
+    public static void enUzunKelimeleriYazdir(ArrayList<String> kelimeler){
+        for (int i = 0; i < kelimeler.size(); i++){
+            System.out.println((i+1) + ". bulunan en uzun kelime: " + kelimeler.get(i));
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Cümle: ");
         String cümle = input.nextLine();
+        String[] kelimeler = temizleVeAyir(cümle);
 
-        // noktalama işaretlerini kaldırma
-        cümle = cümle.replaceAll("[.,'*!?]", "");
-        String[] kelimeler = cümle.split("\\s+");
-
-        // kelime sayma
         System.out.println("Toplam kelime sayısı: " + kelimeler.length);
 
-        // en uzun kelimeyi bulma
-        String enUzunKelime = kelimeler[0];
-        for (int i = 0; i < kelimeler.length; i++) {
-            if (kelimeler[i].length() > enUzunKelime.length()) {
-                enUzunKelime = kelimeler[i];
-            }
-        }
-        System.out.println("En uzun kelime: " + enUzunKelime);
+        enUzunKelimeleriYazdir(enUzunKelimelerBul(kelimeler));
 
         input.close();
     }
