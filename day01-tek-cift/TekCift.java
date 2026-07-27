9c2273c0de93d22f60954d9a39156213acfe11be
Sezer Çetinkaya <sezercetinkaya24@gmail.com>
2026-07-27 23:52:17 +0300

    refactor: day01 tek/cift kontrolu metota tasindi, gecersiz girdide tekrar sorma eklendi

--- day01-tek-cift/TekCift.java
+++ day01-tek-cift/TekCift.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class TekCift {
+
+    public static String tekCiftKontrol(int sayi) {
+        // negatif sayılar içinde çalışıyor o nedenle negatif/pozitif kontrolü yok
+        if (sayi % 2 == 0) {
+            return "Çift";
+        } else {
+            return "Tek";
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Bir sayı giriniz: ");
-        if (input.hasNextInt()) {
-            int sayi = input.nextInt();
-            // yazdığım kontrol negatif sayılar içinde çalışıyor o nedenle negatif/pozitif kontrolü yok
-            if (sayi % 2 == 0) {
-                System.out.println("Çift");
-            } else {
-                System.out.println("Tek");
-            }
-        } else {
-            System.out.println("Sayı girsene lan .xd");
+        while (!input.hasNextInt()) {
+            input.nextLine();
+            System.out.printf("Sayı girsene lan .xd%nYeni sayı: ");
         }
+        int sayi = input.nextInt();
+        System.out.println(tekCiftKontrol(sayi));
 
         input.close();
     }
